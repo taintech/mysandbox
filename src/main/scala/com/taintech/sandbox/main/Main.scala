7c97f4fef26969104feb82ca6ee349559cf65829
rinat.tainov <rinat.tainov@veon.com>
2017-11-08 01:37:32 +0100

    added killswitchs and broadcast hub

--- src/main/scala/com/taintech/sandbox/main/Main.scala
+++ src/main/scala/com/taintech/sandbox/main/Main.scala
@@ -3,7 +3,17 @@ package com.taintech.sandbox.main
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{FileIO, Flow, Keep, Sink, Source}
+import akka.stream.scaladsl.{
+  Broadcast,
+  BroadcastHub,
+  FileIO,
+  Flow,
+  GraphDSL,
+  Keep,
+  RunnableGraph,
+  Sink,
+  Source
+}
 import akka.stream._
 import akka.util.ByteString
 import akka.{Done, NotUsed}
@@ -14,7 +24,6 @@ import scala.concurrent.duration._
 import scalaz._
 import Scalaz._
 
-
 object Main extends App with AkkaStreamsQuickstartGuide {
 
   implicit val actorSystem: ActorSystem = ActorSystem("sandbox")
@@ -23,12 +32,12 @@ object Main extends App with AkkaStreamsQuickstartGuide {
   implicit val executionContext: ExecutionContext = actorSystem.dispatcher
 
   val source: Source[Int, NotUsed] = Source(1 to 100)
-  def done: Future[Done] = source.runForeach(i =>
+  lazy val done: Future[Done] = source.runForeach(i =>
     if (i < 100) println(i) else sys.error("Ouch"))(materializer)
 
   val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
 
-  def result: Future[IOResult] =
+  lazy val result: Future[IOResult] =
     factorials
       .map(num => ByteString(s"$num\n"))
       .runWith(FileIO.toPath(Paths.get("factorials.txt")))
@@ -74,33 +83,69 @@ object Main extends App with AkkaStreamsQuickstartGuide {
 
   // $FiddleDependency org.akka-js %%% akkajsactorstream % 1.2.5.1
 
-
-
   def lineSink(filename: String): Sink[String, Future[IOResult]] =
     Flow[String]
       .map(s => ByteString(s + "\n"))
       .toMat(FileIO.toPath(Paths.get(filename)))(Keep.right)
 
-  def factorials2Result = factorials.map(_.toString).runWith(lineSink("factorial2.txt"))
+  lazy val factorials2Result =
+    factorials.map(_.toString).runWith(lineSink("factorial2.txt"))
 
-  def slowFacts = factorials
+  lazy val slowFacts = factorials
     .zipWith(Source(0 to 10))((num, idx) => s"$idx! = $num")
     .throttle(1, 100.millis, 1, ThrottleMode.shaping)
     .runForeach(println)
 
+//  def writeAuthors: Sink[Author, NotUsed] = ???
+//  def writeHashtags: Sink[Hashtag, NotUsed] = ???
+//  val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+//    import GraphDSL.Implicits._
+//
+//    val bcast = b.add(Broadcast[Tweet](2))
+//    tweets ~> bcast.in
+//    bcast.out(0) ~> Flow[Tweet].map(_.author) ~> writeAuthors
+//    bcast.out(1) ~> Flow[Tweet].mapConcat(_.hashtags.toList) ~> writeHashtags
+//    ClosedShape
+//  })
+//  g.run()
 
+  val zipFutures = (done zip result) zip tweetsDone
 
-  def monadResult = for {
+  val monadResult = for {
     a <- done
     b <- result
     c <- tweetsDone
   } yield (a, b, c)
 
-  val zipFutures = (done zip result) zip tweetsDone
+  val tick =
+    Source.tick(1.seconds, 1.seconds, s"hello ${System.currentTimeMillis()}")
 
-  val applicativeFunctorScalaz = done |@| result |@| tweetsDone |@| factorials2Result |@| slowFacts
+  val broadcastTick: RunnableGraph[Source[String, NotUsed]] =
+    tick.toMat(BroadcastHub.sink(bufferSize = 256))(Keep.right)
+
+  val sharedKillSwitch = KillSwitches.shared("my-kill-switch")
+  val tickResults1 =
+    tick.via(sharedKillSwitch.flow).runForeach(tick => println(s"I : $tick"))
+
+//  val applicativeExp2 = done <*> (result <*> (tweetsDone <*> (factorials2Result <*> (slowFacts map identity))))
 
 //  monadResult.onComplete(_ => actorSystem.terminate())
 //  zipFutures.onComplete(_ => actorSystem.terminate())
-  applicativeFunctorScalaz.e.onComplete(_ => actorSystem.terminate())
+//  applicativeExp.e.onComplete(_ => actorSystem.terminate())
+  val applicativeExp = done |@| result |@| tweetsDone |@| factorials2Result |@| slowFacts |@| tickResults1
+  applicativeExp.ff.onComplete(_ => actorSystem.terminate())
+
+  val scheduled = KillSwitches.shared("my-kill-switch-scheduled")
+  actorSystem.scheduler.scheduleOnce(4.seconds) {
+    tick.via(scheduled.flow).runForeach(tick => println(s"II : $tick"))
+  }
+  actorSystem.scheduler.scheduleOnce(7.seconds) {
+    println("Scheduled kill switch...")
+    scheduled.shutdown()
+  }
+  actorSystem.scheduler.scheduleOnce(10.seconds) {
+    println("Shared kill switch...")
+    sharedKillSwitch.shutdown()
+  }
+
 }
